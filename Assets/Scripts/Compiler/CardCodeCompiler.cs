fdf3eae5346f93cbdf25f36ab524f0d4f8c91a51
leonardo-chagas <leoschagas@hotmail.com>
2022-10-12 23:00:35 -0300

    criação da gramática da linguagem de programação do jogo

---
+++ Assets/Scripts/Compiler/CardCodeCompiler.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+
+public class CardCodeCompiler
+{
+    
+}
---
+++ Assets/Scripts/Compiler/CardCodeProgram.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardCodeProgram
+{
+    #region Program Commands
+    public interface Command{
+        IEnumerator Execute();
+    }
+    #endregion
+
+    
+}
